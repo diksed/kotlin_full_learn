d90e62be74ccc465b810922292966d33da431684
diksed <diksed00@gmail.com>
2023-05-30 11:37:29 +0300

    Integer && Defining && Initialize

--- app/src/main/java/com/sedatkavak/kotlin_full_learn/MainActivity.kt
+++ app/src/main/java/com/sedatkavak/kotlin_full_learn/MainActivity.kt
@@ -7,5 +7,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Integer
+        var x = 5
+        var y = 4
+
+        var age = 35
+        val result = age / 7 * 5
+        println(result)
+
+        // Defining
+        var myInteger : Int
+
+        // Initialize
+        myInteger = 10
+        myInteger = 20
+
+        val a : Int = 23
+        println(a / 2)
     }
 }
