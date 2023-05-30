9ac5aef6f9daff3c8e0c946186fd34ae246adf31
diksed <diksed00@gmail.com>
2023-05-30 11:46:26 +0300

    Double && Float

--- app/src/main/java/com/sedatkavak/kotlin_full_learn/MainActivity.kt
+++ app/src/main/java/com/sedatkavak/kotlin_full_learn/MainActivity.kt
@@ -8,22 +8,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Integer
-        var x = 5
-        var y = 4
-
-        var age = 35
-        val result = age / 7 * 5
-        println(result)
-
-        // Defining
-        var myInteger : Int
-
-        // Initialize
-        myInteger = 10
-        myInteger = 20
-
-        val a : Int = 23
-        println(a / 2)
+        //integer()
+        doubleFloat()
     }
+}
+
+fun integer(){
+    // Integer
+    var x = 5
+    var y = 4
+
+    var age = 35
+    val result = age / 7 * 5
+    println(result)
+
+    // Defining
+    var myInteger : Int
+
+    // Initialize
+    myInteger = 10
+    myInteger = 20
+
+    val a : Int = 23
+    println(a / 2)
+
+    var myLong : Long = 100
+}
+fun doubleFloat(){
+    // Double
+    val pi = 3.14
+
+    // Float
+    val myFloat = 3.14f
+
+    var myAge : Double
+    myAge = 22.0
 }
