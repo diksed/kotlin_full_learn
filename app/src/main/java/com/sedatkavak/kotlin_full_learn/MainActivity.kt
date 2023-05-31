ec73582f3b54d9dab561a83d70b6de66700188c6
diksed <diksed00@gmail.com>
2023-05-31 12:43:12 +0300

    Basic 4 function calculator

--- app/src/main/java/com/sedatkavak/kotlin_full_learn/MainActivity.kt
+++ app/src/main/java/com/sedatkavak/kotlin_full_learn/MainActivity.kt
@@ -6,46 +6,57 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.sedatkavak.kotlin_full_learn.databinding.ActivityMainBinding
 import com.sedatkavak.kotlin_full_learn.kotlin_101.Simpson
 
 class MainActivity : AppCompatActivity() {
-    lateinit var myTextView: TextView
-    lateinit var myButton: Button
-    private lateinit var nameText : EditText
-    private lateinit var ageText : EditText
-    private lateinit var jobText : EditText
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        myTextView = findViewById(R.id.textView)
-        myButton = findViewById(R.id.button)
-        nameText = findViewById(R.id.nameText)
-        ageText = findViewById(R.id.ageText)
-        jobText = findViewById(R.id.jobText)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.plusButton.setOnClickListener(View.OnClickListener {
+            buttonClicked(view, binding.plusButton)
+        })
+        binding.minusButton.setOnClickListener(View.OnClickListener {
+            buttonClicked(view, binding.minusButton)
+        })
+        binding.multiplyButton.setOnClickListener(View.OnClickListener {
+            buttonClicked(view, binding.multiplyButton)
+        })
+        binding.divideButton.setOnClickListener(View.OnClickListener {
+            buttonClicked(view, binding.divideButton)
+        })
 
 
-        val homer = Simpson("Homer", 50, "Nuclear", 70)
-        homer.setHeight(150)
-
-        var myAge : Int? = null
-        // !! -> Null değil
-        // ? -> Null olabilir
-        println(myAge?.minus(10))
-        println(myAge?.minus(10) ?: 0)
     }
 
-    fun mySum(a: Int, b: Int) {
-        myTextView.text = "Result : ${a + b}"
-    }
-    fun myMultiply(a: Int, b: Int): Int {
-        return a * b
-    }
-    fun buttonClicked(view: View) {
-        val name = nameText.text.toString()
-        val age = ageText.text.toString().toIntOrNull() ?: 0
-        val job = jobText.text.toString()
-        val homer = Simpson(name, age, job, 70)
+    fun buttonClicked(view: View, button: Button) {
+        if (binding.firstNumberText.text.isNullOrEmpty() || binding.secondNumberText.text.isNullOrEmpty()) {
+            binding.resultText.text = "Please enter a number"
+            return
+        } else {
+            if (button == binding.plusButton) {
+                val result = binding.firstNumberText.text.toString()
+                    .toDouble() + binding.secondNumberText.text.toString().toDouble()
+                binding.resultText.text = "Result: $result"
+            } else if (button == binding.minusButton) {
+                val result = binding.firstNumberText.text.toString()
+                    .toDouble() - binding.secondNumberText.text.toString().toDouble()
+                binding.resultText.text = "Result: $result"
+            } else if (button == binding.multiplyButton) {
+                val result = binding.firstNumberText.text.toString()
+                    .toDouble() * binding.secondNumberText.text.toString().toDouble()
+                binding.resultText.text = "Result: $result"
+            } else if (button == binding.divideButton) {
+                val result = binding.firstNumberText.text.toString()
+                    .toDouble() / binding.secondNumberText.text.toString().toDouble()
+                binding.resultText.text = "Result: $result"
+            }
+        }
 
-        myTextView.text = "Name : ${homer.name}\nAge : ${homer.age}\nJob : ${homer.job}"
     }
 }
