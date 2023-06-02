06a9a08a3365f9695febe98ebd80a19b3f1812a1
diksed <diksed00@gmail.com>
2023-06-02 13:32:32 +0300

    Storing data with SharedPreferences

--- app/src/main/java/com/sedatkavak/kotlin_full_learn/MainActivity.kt
+++ app/src/main/java/com/sedatkavak/kotlin_full_learn/MainActivity.kt
@@ -1,16 +1,15 @@
 package com.sedatkavak.kotlin_full_learn
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.sedatkavak.kotlin_full_learn.databinding.ActivityMainBinding
-import com.sedatkavak.kotlin_full_learn.kotlin_101.Simpson
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var sharedPref: SharedPreferences
+    var userAgePref : Int? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,45 +17,28 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        binding.plusButton.setOnClickListener(View.OnClickListener {
-            buttonClicked(view, binding.plusButton)
-        })
-        binding.minusButton.setOnClickListener(View.OnClickListener {
-            buttonClicked(view, binding.minusButton)
-        })
-        binding.multiplyButton.setOnClickListener(View.OnClickListener {
-            buttonClicked(view, binding.multiplyButton)
-        })
-        binding.divideButton.setOnClickListener(View.OnClickListener {
-            buttonClicked(view, binding.divideButton)
-        })
-
-
-    }
-
-    fun buttonClicked(view: View, button: Button) {
-        if (binding.firstNumberText.text.isNullOrEmpty() || binding.secondNumberText.text.isNullOrEmpty()) {
-            binding.resultText.text = "Please enter a number"
-            return
-        } else {
-            if (button == binding.plusButton) {
-                val result = binding.firstNumberText.text.toString()
-                    .toDouble() + binding.secondNumberText.text.toString().toDouble()
-                binding.resultText.text = "Result: $result"
-            } else if (button == binding.minusButton) {
-                val result = binding.firstNumberText.text.toString()
-                    .toDouble() - binding.secondNumberText.text.toString().toDouble()
-                binding.resultText.text = "Result: $result"
-            } else if (button == binding.multiplyButton) {
-                val result = binding.firstNumberText.text.toString()
-                    .toDouble() * binding.secondNumberText.text.toString().toDouble()
-                binding.resultText.text = "Result: $result"
-            } else if (button == binding.divideButton) {
-                val result = binding.firstNumberText.text.toString()
-                    .toDouble() / binding.secondNumberText.text.toString().toDouble()
-                binding.resultText.text = "Result: $result"
-            }
+        sharedPref = getSharedPreferences("com.sedatkavak.kotlin_full_learn", MODE_PRIVATE)
+        userAgePref = sharedPref.getInt("age",0)
+        if (userAgePref != 0){
+            binding.resultText.text = "Your Age : $userAgePref"
         }
 
     }
+
+    fun save(view: View) {
+        val myAge = binding.editText.text.toString().toIntOrNull()
+        if (myAge != null) {
+            binding.resultText.text = "Your Age : $myAge"
+            sharedPref.edit().putInt("age", myAge).apply()
+        }
+    }
+
+    fun delete(view: View) {
+        userAgePref = sharedPref.getInt("age",0)
+        if (userAgePref != 0){
+            sharedPref.edit().remove("age").apply()
+            binding.resultText.text = "Your Age : "
+        }
+    }
+
 }
